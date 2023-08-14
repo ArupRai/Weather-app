67feb05ed011900d9ab69718f7afdece7c6bdbc5
Arup Rai <aruprai842@gmail.com>
2023-08-14 23:22:58 +0530

    version -- basic javascript functionality.

--- script.js
+++ script.js
@@ -1,32 +1,29 @@
-const apiKey = '';
+const apiKey = '24269dcd959832c1aa319705994436cd';
 const weatherIcon = document.querySelector('.weatherIcon');
-const errorMsg = document.querySelector('.error');
-const apiUrl = '';
-const searchBar = document.querySelector('.searchBar').value;
+// const errorMsg = document.querySelector('.error');
+const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?&unit=matrix&q=';
+const searchBar = document.querySelector('.searchBar');
 const button = document.querySelector('.searchIcon');
-const wind = document.querySelector('.wind');
-const humidity = document.querySelector('.humidity');
-const temp = document.querySelector('.temperature');
-const city = document.querySelector('.City');
 
 
-
-async function checkWeather($searchBar){
-    const response = await fetch(apiUrl+apiKey+`${searchBar}&unit=matrix`);
-    const data = response.json();
+async function checkWeather(city){
+    const response = await fetch(apiUrl+city+`&appid=${apiKey}`);
+    var data = await response.json();
     if(response.status == 404){
         errorMsg.display ='block';
         // detail.display = 'none';
         // weather.display = 'none';
     }else{
         weatherIcon.src.value = //'/images/{somthing}.png'
-        temp.innerHTML;
-        city.innerHTML;
-        humidity.innerHTML;
-        wind.innerHTML;
+        document.querySelector('.temperature').innerHTML = Math.round(data.main.temp)+" C";
+        document.querySelector('.City').innerHTML = data.name;
+        document.querySelector('.humidity').innerHTML = data.main["humidity"]+"%";
+        document.querySelector('.wind').innerHTML = data.wind.speed+" Km/h";
         errorMsg.display ='none';
         // weather.display = 'block';
         // detail.display = 'block';
     }
 }
-button.addEventListener('click', checkWeather);
+button.addEventListener('click', ()=> {
+    checkWeather(searchBar.value);
+});
